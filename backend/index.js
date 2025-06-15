c92f3e58d1129400d90717d877885ac7984a89ad
Shruti Shaw <shrutishaw750@gmail.com>
2025-06-15 18:19:28 +0530

    Changes for deployment

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
+import path from "path";
 
 const app = express();
 
@@ -23,11 +24,18 @@ app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
 
+const _dirname = path.resolve();
+
 //api's
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
+
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+app.get('*', (_,res) => {
+    res.sendFile(path.resolve(_dirname, "/frontend", "dist", "index.html"));
+})
 
 app.listen(PORT, ()=>{
     connectDB();
